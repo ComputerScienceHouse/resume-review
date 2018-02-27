3400082621efb4da6c435f00f5228a4dded4f377
Ram Zallan <ramzallan@gmail.com>
2018-02-26 21:26:23 -0500

    File previews, enforce PDF filetype on upload

--- routes/resumes.js
+++ routes/resumes.js
@@ -4,26 +4,22 @@ const config = require('../config');
 const db = require('../db');
 const s3 = require('../s3');
 
+function getUrl(id) {
+  var params = {
+      Bucket: config.s3.bucket,
+      Key: id,
+      Expires: 60,
+      ResponseContentType: 'application/pdf',
+  };
+  return s3.getSignedUrl('getObject', params);
+}
+
 router.get('/view/:id', function(req, res, next) {
     db.resumes.find(req.params.id)
     .then(data => {
-        res.render('view', { resume: data });
+        res.render('view', { resume: data, url: getUrl(req.params.id) });
     })
     .catch(error => console.log(error));
-});
-
-router.get('/get/:id', function(req, res, next) {
-  var params = {
-      Bucket: config.s3.bucket,
-      Key: req.params.id,
-      Expires: 60,
-  };
-  s3.getSignedUrl('getObject', params, (error, url) => {
-      if (error) {
-          res.send('Could not find file with ID: ' + req.params.id);
-      }
-      res.redirect(url);
-  });
 });
 
 module.exports = router;
