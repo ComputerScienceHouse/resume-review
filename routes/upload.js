e0f6780f828efbc382e8e8524a2d347dda48bbba
Ram Zallan <ramzallan@gmail.com>
2018-03-23 17:33:47 -0400

    Handle uploading a preexisting file

--- routes/upload.js
+++ routes/upload.js
@@ -15,37 +15,51 @@ router.get('/',
 router.post('/',
   upload.single('resume'),
   (req, res) => {
+    // verify file is a PDF
     if (req.file.mimetype !== 'application/pdf') {
       res.status(400).send('Upload a PDF');
       return;
     }
     var id = hasha(req.file.buffer, {algorithm: 'md5'});
-    var filename = req.file.originalname;
-    var author = req.user._json.preferred_username;
-    var date = new Date().toISOString().slice(0, 19).replace('T', ' '); // sql format
-    db.resumes.add({
-      id: id,
-      filename: filename,
-      author: author,
-      date: date,
-    }).then(() => {
-      let params = {
-        Bucket: config.s3.bucket,
-        Key: id,
-        Body: req.file.buffer,
-        ContentType: 'application/pdf',
-      };
-      s3.putObject(params, function(error, data) {
-        if (error) {
-          res.send('Could not upload file');
-          console.log('Could not upload file');
-        } else {
-          res.redirect('/resumes/view/' + id);
-        }
-      })
-    }).catch(() => {
+    // check if resume is already uploaded
+    db.resumes.find(id)
+    .then((data) => {
+      if (data) {
+        res.status(400).send('File already uploaded. <a href="/resumes/view/' + id + '">View it here</a>');
+        return;
+      }
+      // if not found, upload it
+      var filename = req.file.originalname;
+      var author = req.user._json.preferred_username;
+      var date = new Date().toISOString().slice(0, 19).replace('T', ' '); // sql format
+      // add to DB
+      db.resumes.add({
+        id: id,
+        filename: filename,
+        author: author,
+        date: date,
+      }).then(() => {
+        // add to S3
+        let params = {
+          Bucket: config.s3.bucket,
+          Key: id,
+          Body: req.file.buffer,
+          ContentType: 'application/pdf',
+        };
+        s3.putObject(params, function(error, data) {
+          if (error) {
+            res.send('Could not upload file');
+            console.log('Could not upload file');
+          } else {
+            res.redirect('/resumes/view/' + id);
+          }
+        })
+      }).catch(() => {
+        res.send('Could not upload file');
+      });
+    })
+    .catch((error) => {
       res.send('Could not upload file');
-      console.log('Could not upload file');
     });
   });
 
