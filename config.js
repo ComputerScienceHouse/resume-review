d3ac38fd6502003d61d3a1cd205eef355ea1f0cc
Ram Zallan <ramzallan@gmail.com>
2018-04-05 21:11:33 -0400

    Update config variable reading

---
+++ config.js
@@ -0,0 +1,22 @@
+const config = {};
+
+config.port = process.env.PORT || '4200';
+
+config.db = {};
+config.db.name = process.env.DB_NAME;
+config.db.username = process.env.DB_USERNAME;
+config.db.password = process.env.DB_PASSWORD;
+
+config.s3 = {};
+config.s3.accessKeyId = process.env.S3_ACCESS_KEY_ID;
+config.s3.secretAccessKey = process.env.S3_SECRET_ACCESS_KEY;
+config.s3.url = process.env.S3_URL || 'https://s3.csh.rit.edu';
+config.s3.bucket = process.env.S3_BUCKET || 'ram_resumes';
+
+config.auth = {};
+config.auth.client_id = process.env.OIDC_CLIENT_ID;
+config.auth.client_secret = process.env.OIDC_CLIENT_SECRET;
+config.auth.callback_url = process.env.OIDC_CALLBACK_URL;
+config.auth.session_secret = process.env.OIDC_SESSION_SECRET;
+
+module.exports = config;
