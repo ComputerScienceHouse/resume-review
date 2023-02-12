d3146c0f295259d34c926ebc512e630fa0af2c1c
Lonnie Gerol <lonnie@lonniegerol.com>
2023-02-11 20:14:58 -0500

    Fixes bug where newer s3 buckets couldn't be written to

--- s3.js
+++ s3.js
@@ -3,9 +3,10 @@ const config = require('./config');
 
 const s3 = new AWS.S3({
     endpoint: config.s3.url,
+    s3ForcePathStyle: true,
     accessKeyId: config.s3.accessKeyId,
     secretAccessKey: config.s3.secretAccessKey,
+    region: 'us-east-1'
 });
-
 
 module.exports = s3;
