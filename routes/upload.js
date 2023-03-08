3fa7e32494634e1137cc63236feb163f6acf3d9c
Joe Abbate <josephabbateny@gmail.com>
2023-03-07 19:57:04 -0500

    Catch pdfparse Error

--- routes/upload.js
+++ routes/upload.js
@@ -36,47 +36,52 @@ router.post('/',
       res.status(400).render('upload', { user: req.user._json, error: 'Really? "resume.pdf"? C\'mon.' });
       return;
     }
-    parsePdf(req.file.buffer).then(data => console.log(data.text));
-    var id = hasha(req.file.buffer, {algorithm: 'md5'});
-    // check if resume is already uploaded
-    db.resumes.find(id)
-    .then((data) => {
-      if (data) {
-        res.status(400).send('File already uploaded.');
-        return;
-      }
-      // if not found, upload it
-      var filename = req.body.title || req.file.originalname;
-      var authorUsername = req.user._json.preferred_username;
-      var date = new Date().toISOString().slice(0, 19).replace('T', ' '); // sql format
-      // add to DB
-      db.resumes.add({
-        id: id,
-        filename: filename,
-        author: authorUsername,
-        date: date,
-      }).then(() => {
-        // add to S3
-        let params = {
-          Bucket: config.s3.bucket,
-          Key: id,
-          Body: req.file.buffer,
-          ContentType: 'application/pdf',
-        };
-        s3.putObject(params, function(error, data) {
-          if (error) {
-            res.send('Could not upload file');
-            console.log('Could not upload file');
-          } else {
-            res.redirect('/resumes/view/user/' + authorUsername);
-          }
-        })
-      }).catch(() => {
-        res.send('Could not upload file');
-      });
-    })
-    .catch((error) => {
-      res.send('Could not upload file');
+    parsePdf(req.file.buffer)
+    .then(_ => {
+      var id = hasha(req.file.buffer, {algorithm: 'md5'});
+      // check if resume is already uploaded
+      db.resumes.find(id)
+      .then((data) => {
+        if (data) {
+          res.status(400).send('File already uploaded.');
+          return;
+        }
+        // if not found, upload it
+        var filename = req.body.title || req.file.originalname;
+        var authorUid = req.user._json.sub;
+        var date = new Date().toISOString().slice(0, 19).replace('T', ' '); // sql format
+        // add to DB
+        db.resumes.add({
+          id: id,
+          uid: authorUid,
+          filename: filename,
+          date: date,
+        }).then(() => {
+          // add to S3
+          let params = {
+            Bucket: config.s3.bucket,
+            Key: id,
+            Body: req.file.buffer,
+            ContentType: 'application/pdf',
+          };
+          s3.putObject(params, function(error, data) {
+            if (error) {
+              res.send(`Could not upload file: ${error}`);
+              console.log('Could not upload file');
+            } else {
+              res.redirect('/resumes/view/user/' + authorUid);
+            }
+          })
+        }).catch((error) => {
+          res.send(`Could not upload file: ${error}`);
+        });
+      })
+      .catch((error) => {
+        res.send(`Could not upload file: ${error}`);
+      })
+      })
+    .catch(e => {
+      res.status(500).render('upload', { user: req.user._json, error: `${e.name}: ${e.message}` });
     });
   });
 
